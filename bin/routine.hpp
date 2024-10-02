37a7c628078af994e9be5e20c8f890b1c36a6388
Segward <gustask@stud.ntnu.no>
2024-10-02 10:02:42 +0200

    Started on thread hijack but it doesnt work

--- bin/routine.hpp
+++ bin/routine.hpp
@@ -107,3 +107,80 @@ BOOL Routine::CreateRemoteThreadInject() {
 
     return TRUE;
 }
+
+BOOL Routine::HijackRemoteThreadInject() {
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+    if (hSnap == INVALID_HANDLE_VALUE) {
+        std::cerr << "Error: CreateToolhelp32Snapshot failed" << std::endl;
+        return FALSE;
+    }
+
+    THREADENTRY32 te32;
+    te32.dwSize = sizeof(te32);
+    if (!Thread32First(hSnap, &te32)) {
+        std::cerr << "Error: Thread32First failed" << std::endl;
+        return FALSE;
+    }
+
+    HANDLE hThread = 0;
+    do {
+        hThread = te32.th32OwnerProcessID == pid ? 
+            OpenThread(THREAD_ALL_ACCESS, FALSE, te32.th32ThreadID) : 0;
+    } while (Thread32Next(hSnap, &te32) && hThread == 0);
+
+    CloseHandle(hSnap);
+
+    if (hThread == 0) {
+        std::cerr << "Error: Thread not found" << std::endl;
+        return FALSE;
+    }
+
+    LPCONTEXT context;
+    BOOL fetched = GetThreadContext(hThread, context);
+    this->lastError = GetLastError();
+    if (this->lastError != 0) {
+        std::cerr << "Failed to get thread context: " << this->lastError << std::endl;
+        CloseHandle(hProcess);
+        return FALSE;
+    }
+
+    context->ContextFlags = CONTEXT_FULL;
+    
+    #ifdef _WIN64
+        if (context->Rip == 0) {
+            CloseHandle(hProcess);
+            std::cerr << "RIP is 0" << std::endl;
+            return FALSE;
+        }
+    #elif _WIN32 
+        if (context->Eip == 0) {
+            CloseHandle(hProcess);
+            std::cerr << "EIP is 0" << std::endl;
+            return FALSE;
+        }
+    #endif
+
+    PVOID location = VirtualAllocEx(hProcess, 0, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    this->lastError = GetLastError();
+    if (this->lastError != 0) {
+        std::cerr << "Failed to allocate memory: " << this->lastError << std::endl;
+        CloseHandle(hProcess);
+        return FALSE;
+    }
+
+    BOOL written = WriteProcessMemory(hProcess, location, dllPath, strlen(dllPath) + 1, 0);
+    this->lastError = GetLastError();
+    if (this->lastError != 0) {
+        std::cerr << "Failed to write memory: " << this->lastError << std::endl;
+        VirtualFreeEx(hProcess, location, 0, MEM_RELEASE);
+        CloseHandle(hProcess);
+        return FALSE;
+    }
+
+    if (hProcess)
+        CloseHandle(hProcess);
+
+    
+    
+    return TRUE;
+}
